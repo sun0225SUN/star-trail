1591b805019435c25c1eb406c33d296362c294ab
guoqi.sun <sun412926@gmail.com>
2024-02-13 10:09:11 +0800

    :sparkles: feat: lint code

--- uno.config.js
+++ uno.config.js
@@ -22,7 +22,7 @@ export default defineConfig({
     ['wh-full', 'w-full h-full'],
     ['f-b-c', 'flex justify-between items-center'],
     ['f-c-c', 'flex justify-center items-center'],
-    ['flex-c', 'flex flex-col'],
+    ['flex-col', 'flex flex-col'],
     ['icon-btn', 'text-5 inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-primary !outline-none'],
   ],
 })
