f50f6cc9ab9c56a185d1bbcf4cd591f4fa6db3b6
guoqi.sun <sun412926@gmail.com>
2024-02-14 01:22:04 +0800

    :sparkles: feat: add random title

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,20 @@ import { onMounted, reactive } from 'vue'
 import StarTrails from './components/StarTrails.vue'
 
 const data = reactive({
+  titleList: [
+    '希望能成为有趣的人',
+    '给时光以生命<br>给岁月以文明',
+    '你好，请多指教',
+    '一望无际的迷雾中，<br/>有人在寻找光明！',
+    '你所热爱的<br>就是你的生活',
+    '当你在凝视着网页的时候<br>网页也正在凝视着你',
+    '热爱……',
+    '平凡的日常正奇迹的发生着',
+    '敬畏之心！',
+    '赞美之心！',
+    '我很好奇！',
+    '*舒缓的现代音乐*',
+  ],
   navLinks: [{
     name: '博 客',
     link: 'https://blog.sunguoqi.com',
@@ -102,8 +116,7 @@ onMounted(() => {
     <!-- 大标题 -->
     <section ml-15vw absolute>
       <div text-10 text-white font-bold tracking-widest>
-        你所热爱的<br>
-        就是你的生活
+        {{ data.titleList[Math.floor(Math.random() * data.titleList.length)] }}
       </div>
       <div flex items-center>
         <div mr-4 flex gap-2>
@@ -190,7 +203,7 @@ onMounted(() => {
         <p>路虽远行则将至，事虽难做则必成</p>
         <i i-ant-design-environment-outlined ml-1 />
       </div>
-      <div mt-2 f-c-c class="text-white/60" gap-4>
+      <div class="text-white/60" mb-3 mt-2 f-c-c gap-4>
         <div>皖 ICP 备 2021010058 号</div>
         <div>© 2024 小孙同学</div>
       </div>
